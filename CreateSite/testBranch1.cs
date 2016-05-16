794e011ee75f7a4b9c58ee5986fd8d95cd896e0d
baronhq <26222521@qq.com>
2016-05-16 17:03:45 +0800

    1

---
+++ CreateSite/testBranch1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateSite
+{
+    class testBranch1
+    {
+        string testBranchType = "testBranch";
+
+        string master = "master";
+
+        string testBranchType2 = "testBranchType2";
+
+        string testBranchType3 = "testBranchType3";
+
+        string testBranchType4 = "testBranchType4";
+
+        string testBranchType5 = "testBranchType5";
+
+        string testBranchType6 = "testBranchType6";
+
+        string testBranchType7 = "testBranchType7";
+
+        string testBranchType8 = "testBranchType8";
+        
+        string testBranchType9 = "testBranchType9";
+
+        string testBranchType10 = "testBranchType10";
+
+        string testBranchType12 = "testBranchType12";
+    }
+}
