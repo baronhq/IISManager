4c4bf55b159dfe24e1337d22152bdc591181a1b9
baronhq <26222521@qq.com>
2016-05-18 17:07:46 +0800

    iis测试modules

---
+++ IISManagerMvc/App_Code/PermissionModule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IISManagerMvc.App_Code
+{
+    public class PermissionModule : IHttpModule
+    {
+
+        public void Init(HttpApplication context)
+        {
+            context.AcquireRequestState += new EventHandler(context_AcquireRequestState);
+
+            context.AuthorizeRequest += new EventHandler(context_AuthorizeRequest);
+
+            context.AuthenticateRequest += new EventHandler(context_AuthenticateRequest);
+        }
+
+        private void context_AuthenticateRequest(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void context_AuthorizeRequest(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void context_AcquireRequestState(object sender, EventArgs e)
+        {
+           
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
